0fd63207ae7c77a021c668143cb2190379e534d9
thiago2santos <thisanti@hotmail.com>
2025-09-03 11:06:20 -0300

    fix: resolve unnecessary stubbing in CreateWalletCommandHandlerTest

--- src/test/java/com/wallet/application/handler/CreateWalletCommandHandlerTest.java
+++ src/test/java/com/wallet/application/handler/CreateWalletCommandHandlerTest.java
@@ -16,6 +16,8 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import java.math.BigDecimal;
 
@@ -24,6 +26,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class CreateWalletCommandHandlerTest {
 
     @InjectMocks
